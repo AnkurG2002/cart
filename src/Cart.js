4c1c558391a45f92d561c0731bbe77339f0f35a4
Ankur <ankurgoswami718@gmail.com>
2023-03-21 00:02:13 +0530

    Added Delete Product from Cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,110 @@
 import React from "react";
 import CartItem from './CartItem'
 
+// The extends keyword is used to create a child class of another class (parent). The child class inherits all the methods from another class.
 class Cart extends React.Component {
+    constructor() {
+        // In JS, whenever you are inheriting from parent class, we first need to call the constructor of the parent class, this is done by calling super()
+        super();
+        this.state = {
+            products: [
+                {
+                    item: 'Mobile',
+                    price: 14999,
+                    qty: 1,
+                    img: '',
+                    id: 1
+                },
+                {
+                    item: 'Watch',
+                    price: 2999,
+                    qty: 1,
+                    img: '',
+                    id: 2
+                },
+                {
+                    item: 'Laptop',
+                    price: 45999,
+                    qty: 1,
+                    img: '',
+                    id: 3
+                },
+            ]
+        }
+    }
+
+
+    // Unlike regular functions, arrow functions do not have their own 'this'. 
+    // The value of 'this' inside an arrow function remains the same throughout the lifecycle of the function
+    // and is always bound to the value of 'this' in the closest non-arrow parent function.
+
+    // Arrow Function
+    handleIncreaseQunatity = (product) => {
+        // this.state.qty++;             // this will only change qty in our state object, React doesn't refresh or re-render our component so it doesn't get reflected in JSX
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+        // setState method-1
+        // this.setState(() => {
+        //     return {
+        //         products: products
+        //     }
+        // })
+
+        // setstate method-2
+        this.setState({
+            // products: products
+
+            // using short-hand property, as key & value are same
+            products
+        });
+    } 
+
+    // Regular Function
+    handleDecreaseQunatity(product) {
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        if(products[index].qty === 0){
+            return;
+        }
+        products[index].qty -= 1;
+        this.setState({
+            products: products
+        });
+    }
+
+    handleDeleteProduct = (id) => {
+        const { products } = this.state;
+        const items = products.filter((item) => {  // this will return an array that will contain products (item) whose id != given id
+            return item.id !== id;
+        })
+        this.setState({
+            products: items
+        })
+    }
+
     render() {
+        const { products } = this.state;
         return (
             <div className="cart"> 
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                {/* these qty, price, title img are props */}
+                {/* <CartItem qty={1} price={999} item={"Watch"} img={''} /> */}
+                
+                {
+                    products.map((prod) => {
+                        return (
+                            <CartItem 
+                                product={prod} 
+                                key={prod.id} 
+                                onIncreaseQuantity={this.handleIncreaseQunatity}  // using arrow function, no binding required.
+                                onDecreaseQuantity={this.handleDecreaseQunatity.bind(this)} // if we are using regular function we have to bind our function reference with 'this', else 'this' will get lost and will be undefined, because it is getting internally called via React, and not via any Object. 
+                                onDeleteProduct={this.handleDeleteProduct}
+                            />
+                        );
+                    })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,58 +1,18 @@
 import React from "react";
 
-// The extends keyword is used to create a child class of another class (parent). The child class inherits all the methods from another class.
 class CartItem extends React.Component {
-    constructor() {
-        // In JS, whenever you are inheriting from parent class, we first need to call the constructor of the parent class, this is done by calling super()
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-    }
-    // Unlike regular functions, arrow functions do not have their own 'this'. 
-    // The value of 'this' inside an arrow function remains the same throughout the lifecycle of the function
-    // and is always bound to the value of 'this' in the closest non-arrow parent function.
-    IncreaseQuantity = () => {
-        // this.state.qty++;             // this will only change qty in our state object, React doesn't refresh or re-render our component so it doesn't get reflected in JSX
-        // console.log(this.state.qty);
-
-
-        // setState is used to update and re-render our component
-        // setState method-1
-        // this.setState((prevState) => {
-        //     return {
-        //         qty: prevState.qty + 1
-        //     }
-        // })
-
-        // setState method-2
-        this.setState({ 
-            qty: this.state.qty + 1
-        });
-
-    }
-    DecreaseQuantity = () => {
-        if(this.state.qty === 0){
-            return;
-        }
-        this.setState({
-            qty: this.state.qty - 1
-        });
-    }
     render() {
         // object destructuring: used to unpack values from arrays, or properties from objects, into distinct variables.
-        const { price, title, qty } = this.state;
-        
+        const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = this.props;  // props received from Cart Component
+        const { item, price, qty } = this.props.product;  
+
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }> {title} </div>
+                    <div style={ {fontSize: 25} }> {item} </div>
                     <div style={ {color: '#777'} }> ₹ {price} </div>
                     <div  style={ {color: '#777'} }> Qty: {qty} </div>
                     <div className="cart-item-actions">
@@ -61,19 +21,19 @@ class CartItem extends React.Component {
                             alt="increase" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/512/4315/4315609.png" 
-                            // onClick={this.IncreaseQuantity.bind(this)} // We have to bind our function reference with 'this', else 'this' will get lost and will be undefined, because it is getting internally called via React, and not via any Object. 
-                            onClick={this.IncreaseQuantity} // Another way to solve this problem is to use arrow function
+                            onClick={() => onIncreaseQuantity(product)}
                         />
                         <img 
                             alt="decrease" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/512/7080/7080604.png" 
-                            onClick={this.DecreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
                         />
                         <img 
                             alt="delete" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/512/484/484560.png" 
+                            onClick={() => onDeleteProduct(product.id)}
                         />
                     </div>
                 </div>
